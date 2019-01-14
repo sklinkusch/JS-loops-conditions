e1310e9b6bbf1b4c8b5421aae557a603613b544f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-14 09:32:39 +0100

    added function with ternary operator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // First version of function with if statements
-function LoopDivisor() {
+function LoopDivisorIf() {
   // Loop from 1 to 100
   for (let loopVar = 1; loopVar < 101; loopVar++) {
     if (loopVar % 3 === 0 && loopVar % 5 !== 0) {
@@ -17,4 +17,20 @@ function LoopDivisor() {
     }
   }
 }
-LoopDivisor();
+// Second version of function (ternary operator)
+function LoopDivisorTernary() {
+  for (let loopVar = 1; loopVar < 101; loopVar++) {
+    loopVar % 3 === 0 && loopVar % 5 !== 0
+      ? console.log("Happy")
+      : loopVar % 5 === 0 && loopVar % 3 !== 0
+      ? console.log("Sad")
+      : loopVar % 3 === 0 && loopVar % 5 === 0
+      ? console.log("Both")
+      : loopVar % 2 === 0
+      ? console.log("Furious")
+      : loopVar % 11 === 0 || loopVar % 7 === 0
+      ? console.log("Lonely")
+      : console.log(loopVar);
+  }
+}
+LoopDivisorTernary();
