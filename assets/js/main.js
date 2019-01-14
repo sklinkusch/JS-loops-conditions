3dd77a3ac53d59987996484917d5dc352a187619
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-14 09:24:47 +0100

    created first version of function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 function LoopDivisor() {
   // Loop from 1 to 100
   for (let loopVar = 1; loopVar < 101; loopVar++) {
-    console.log(loopVar);
-    if (loopVar % 3 === 0) {
+    if (loopVar % 3 === 0 && loopVar % 5 !== 0) {
       console.log("Happy");
+    } else if (loopVar % 5 === 0 && loopVar % 3 !== 0) {
+      console.log("Sad");
+    } else if (loopVar % 5 === 0 && loopVar % 3 === 0) {
+      console.log("Both");
+    } else if (loopVar % 2 === 0) {
+      console.log("Furious");
+    } else if (loopVar % 11 === 0 || loopVar % 7 === 0) {
+      console.log("Lonely");
+    } else {
+      console.log(loopVar);
     }
   }
 }
