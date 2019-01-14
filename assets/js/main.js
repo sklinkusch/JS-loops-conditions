51dccb799fec3c30c73e273b81fe881448487ab6
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-14 09:07:43 +0100

    added first if statement

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,9 @@ function LoopDivisor() {
   // Loop from 1 to 100
   for (let loopVar = 1; loopVar < 101; loopVar++) {
     console.log(loopVar);
+    if (loopVar % 3 === 0) {
+      console.log("Happy");
+    }
   }
 }
+LoopDivisor();
